60f850934924dde3b4161323a28244b856b81b36
Akash Singh <mr.singhaksh@gmail.com>
2023-10-04 19:22:11 +0530

    feat:Add contact us and footer section

--- src/App.tsx
+++ src/App.tsx
@@ -4,6 +4,8 @@ import { SelectedPage } from "./shared/types"
 import Home from "./scenes/home"
 import Benefits from "./scenes/benefits"
 import OurClasses from "./scenes/ourClasses"
+import ContactUs from "./scenes/contactUs"
+import Footer from "./scenes/footer"
 
 function App() {
   
@@ -28,6 +30,8 @@ function App() {
     <Home setSelectedPage={setSelectedPage}/>
     <Benefits setSelectedPage={setSelectedPage} />
     <OurClasses setSelectedPage={setSelectedPage}/>
+    <ContactUs setSelectedPage={setSelectedPage}/>
+    <Footer/>
     </div>
   )
 }
